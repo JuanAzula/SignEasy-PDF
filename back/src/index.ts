bbf11f4111dc7d93c171a283b0c2e60460b3653f
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-06-03 15:29:55 +0200

    Remove unused consts

--- back/src/index.ts
+++ back/src/index.ts
@@ -5,6 +5,6 @@ import { pino } from 'pino';
 const logger = pino();
 
 const PORT = env.PORT || 3333
-const server = app.listen(PORT, () =>
+app.listen(PORT, () =>
     logger.info(`[server]: Server is running at http://localhost:${PORT}`)
-) 
+)
